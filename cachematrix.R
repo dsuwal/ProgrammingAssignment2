d19bd8b2988d260859d9c5e40d29a1a90bc1326d
dsuwal <dsuwal@veriskhealth.com>
2016-03-13 18:27:58 -0600

    Solution to the Week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## The following function creates a matrix object which caches its inverse.
+ 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## The following function computes the inverse of the matrix created by the function makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then it will retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("Fatched from cache.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setinverse(inv)
+    inv
 }
+
+##Testing above functions 
+##> m <- matrix(1:4, 2,2)
+##> m
+##     [,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+##> cm <- makeCacheMatrix(m)
+##> cm$get()
+##     [,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+##> cacheSolve(cm)
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##> cacheSolve(cm)
+##Fatched from cache.
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##> 
